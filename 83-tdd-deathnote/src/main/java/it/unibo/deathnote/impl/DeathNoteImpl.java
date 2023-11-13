2c4a61123c07fa9abfcfd2d9793973c860509be4
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2023-11-13 13:02:02 +0100

    override of methods in the interface

--- 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
+++ 83-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -1,19 +1,28 @@
 package it.unibo.deathnote.impl;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 import it.unibo.deathnote.api.DeathNote;
 
 public class DeathNoteImpl implements DeathNote {
 
+    private List<String> names = new ArrayList<>();
+
     @Override
-    public String getRule(int ruleNumber) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRule'");
+    public String getRule(int ruleNumber) {   
+        if(ruleNumber >= 1 && ruleNumber <= RULES.size()) {
+            return RULES.get(ruleNumber);
+        }
+        throw new IllegalArgumentException("rule number out of bounds");
     }
 
     @Override
     public void writeName(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeName'");
+        if(!Objects.isNull(name)) {
+
+        }
+        throw new NullPointerException("non existing name"); 
     }
 
     @Override
